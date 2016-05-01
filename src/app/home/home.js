cf941122905e882dc893d492148ed310ac0f003c
David Tyler <davet.code@gmail.com>
2016-05-01 12:02:02 +0100

    Fake up some data for the home page to play with

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -1,25 +1,37 @@
 function homeConfig($stateProvider) {
-  $stateProvider.state( 'home', {
-    url: '/home',
+  $stateProvider.state( 'app.home', {
+    url: 'home',
     views: {
       'main': {
-        controller: 'HomeCtrl',
+        controller: 'homeController',
         controllerAs: 'home',
         templateUrl: 'home/home.tpl.html'
       }
     },
     data: { pageTitle: 'Home' },
     resolve: {
-      'data': function($stateParams, dataFactory) {
-        return dataFactory.get('test');
+      'stories': function() {
+        return [
+          new Story('Test Story 1', 'This is a blurb for the test story #1'),
+          new Story('Test Story 2', 'This is a blurb for the test story #2'),
+          new Story('Test Story 3', 'This is a blurb for the test story #3'),
+          new Story('Test Story 4', 'This is a blurb for the test story #4'),
+          new Story('Test Story 5', 'This is a blurb for the test story #5')
+        ];
+      },
+      'unassignedSections': function () {
+        return [
+          new Section('A loose section', 'This is some _markdown_ defined __content__')
+        ]
       }
     }
   });
 }
 
 class HomeController {
-  constructor(data) {
-    this.data = data;
+  constructor(stories, unassignedSections) {
+    this.stories = stories;
+    this.unassignedSections = unassignedSections;
   }
 }
 
