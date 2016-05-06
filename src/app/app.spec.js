d3d54f6882034d04eb5da12e23dabbd6956eba56
David Tyler <davet.code@gmail.com>
2016-05-06 08:12:22 +0100

    Rename unit tests to story book

--- src/app/app.spec.js
+++ src/app/app.spec.js
@@ -2,7 +2,7 @@ describe( 'AppCtrl', function() {
   describe( 'dummyTest', function() {
     var AppCtrl, $location, $scope;
 
-    beforeEach( module( 'angularTemplate' ) );
+    beforeEach( module( 'story-book' ) );
 
     beforeEach( inject( function( $controller, _$location_, $rootScope ) {
       $location = _$location_;
